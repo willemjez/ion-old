f77b9ed5cb8eed21dcf6af318ecba98c078912b8
aspaas <aspa@servitising.org>
2017-07-30 14:45:28 +0200

    🏔 Ref #64 Updated file permissions: removed many +x flags. Removed 2 empty, unneeded directories.
    Former-commit-id: 12c1bdb7f6fd9d03126c67fda5f20ba0183703a5

--- contrib/static-build-scripts/build_unix_static.sh
+++ contrib/static-build-scripts/build_unix_static.sh
@@ -1,4 +1,8 @@
-cd ../../
-./autogen.sh;
+#!/bin/bash
 
-qmake;./configure --enable-glibc-back-compat --prefix=`pwd`/depends/x86_64-pc-linux-gnu LDFLAGS="-static-libstdc++";make
+cd ../depends
+make HOST=x86_64-pc-linux-gnu
+cd ..
+./autogen.sh
+./configure --enable-glibc-back-compat --prefix=`pwd`/depends/x86_64-pc-linux-gnu LDFLAGS="-static-libstdc++"
+make
