fdae64fc57f99464bd93aa855fa950afa6398297
cevap <dev@i2pmail.org>
2017-07-31 23:47:05 +0200

    🐼 - 🛁 fix #71
    Former-commit-id: 3fc7d498352f8343d2bdabbf93592213ad085048

--- build/2.1.6.1/ppaprepare.sh
+++
@@ -1,4 +0,0 @@
-sha1sum ion* > sha1sum
-sha256sum ion* > sha256sum
-sha512sum ion* > sha512sum
-md5sum ion* > md5sum
--- build/scripts/create-ion-bin-hashes.sh
+++
@@ -1,9 +0,0 @@
-sha1sum ./iond > iond.sha1sum
-sha256sum ./iond > iond.sha256sum
-sha512sum ./iond > iond.sha512sum
-md5sum ./iond > iond.md5sum
-
-sha1sum ./ion-qt > ion-qt.sha1sum
-sha256sum ./ion-qt > ion-qt.sha256sum
-sha512sum ./ion-qt > ion-qt.sha512sum
-md5sum ./ion-qt > ion-qt.md5sum
